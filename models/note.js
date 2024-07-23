c5e30037a5b131e191c21c627a5968ebab5245ac
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-07-23 15:09:56 -0700

    fix .env position

--- models/note.js
+++ models/note.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 
 const noteSchema = new mongoose.Schema({
-    text: {type: String, required: true},
-    isRead: {type: Boolean, required: true},
+    text: { type: String, required: true },
+    isRead: { type: Boolean, required: true }
 },{
-    timestamps: true
+  timestamps: true  
 })
 
 const Note = mongoose.model('Note', noteSchema)
